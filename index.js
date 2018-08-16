1094962d6dd16286f72575ab3060e9b61f6401d0
Alisson Zampietro <Alisson Zampietro>
2018-08-16 12:26:46 -0300

    feat: including routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = requie('express');
 const app = express();
+const routes = require('./src/routes');
 
+routes(app);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+
+class Routes {
+    constructor(app) {
+        console.log(app);
+    }
+}
+
+
+module.exports = app => Routes(app);
